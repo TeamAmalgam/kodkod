4584faa73250a37907de9e92686b5ddae30305e9
Ming-Ho Yee <mh@mhyee.com>
2013-07-18 10:57:25 -0400

    DO NOT MERGE INTO MASTER - WIP PartitionedGIA
    This is an attempt to implement PGIA using a ForkJoinPool. However, it
    does not work. Committing and pushing only for safekeeping.
    
    [ci skip]

--- src/kodkod/multiobjective/algorithms/PartitionedGuidedImprovementAlgorithm.java
+++ src/kodkod/multiobjective/algorithms/PartitionedGuidedImprovementAlgorithm.java
@@ -1,6 +1,7 @@
 package kodkod.multiobjective.algorithms;
 
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -9,6 +10,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -40,9 +43,6 @@ public class PartitionedGuidedImprovementAlgorithm extends MultiObjectiveAlgorit
         //begin, amongst others, start the timer
         begin();
 
-        final List<Formula> exclusionConstraints = new ArrayList<Formula>();
-        exclusionConstraints.add(problem.getConstraints());
-
         // Throw a dart and get a starting point
         Solution solution = getSolver().solve(problem.getConstraints(), problem.getBounds());
         incrementStats(solution, problem, problem.getConstraints(), true, null);
@@ -53,65 +53,18 @@ public class PartitionedGuidedImprovementAlgorithm extends MultiObjectiveAlgorit
 
         // Find the boundaries of the search space
         Formula boundaryConstraints = findBoundaries(problem, startingValues);
-        exclusionConstraints.add(boundaryConstraints);
 
-        IncrementalSolver solver = IncrementalSolver.solver(getOptions());
+        // Create the ForkJoinPool
+        // Number of threads is MIN(user value, # cores)
+        // TODO: Make "user value" configurable
+        int poolSize = Math.min(8, Runtime.getRuntime().availableProcessors());
+        ForkJoinPool pool = new ForkJoinPool(poolSize);
 
-        // Restart the search with an IncrementalSolver
-        // We want the boundaries and problem constraints in the solver, before incrementally adding improvement constraints
-        Formula constraint = Formula.and(exclusionConstraints);
-        solution = solver.solve(constraint, problem.getBounds());
-        incrementStats(solution, problem, constraint, false, constraint);
-        counter.countStep();
+        // Create the first task and submit it to the pool
+        PartitionSpaceTask task = new PartitionSpaceTask(problem, boundaryConstraints, notifier);
 
-        // While the current solution is satisfiable try to find a better one
-        while (isSat(solution)) {
-            MetricPoint currentValues = null;
-            Solution previousSolution = null;
-
-            // Work up to the Pareto front
-            while (isSat(solution)) {
-                currentValues = MetricPoint.measure(solution, problem.getObjectives(), getOptions());
-                logger.log(Level.FINE, "Found a solution. At time: {0}, Improving on {1}", new Object[] { Integer.valueOf((int)(System.currentTimeMillis()-startTime)/1000), currentValues.values() });
-
-                final Formula improvementConstraints = currentValues.parametrizedImprovementConstraints();
-
-                previousSolution = solution;
-                solution = solver.solve(improvementConstraints, new Bounds(problem.getBounds().universe()));
-                incrementStats(solution, problem, improvementConstraints, false, improvementConstraints);
-
-                counter.countStep();
-            }
-            foundParetoPoint(currentValues);
-
-            // Free the solver's resources since we will be creating a new solver
-            solver.free();
-
-            if (!options.allSolutionsPerPoint()) {
-                // no magnifying glass
-                // previous solution was on the pareto front: report it
-                tell(notifier, previousSolution, currentValues);
-            } else {
-                // magnifying glass
-                final Collection<Formula> assignmentsConstraints = currentValues.assignmentConstraints();
-                assignmentsConstraints.add(problem.getConstraints());
-                int solutionsFound = magnifier(Formula.and(assignmentsConstraints), problem.getBounds(), currentValues, notifier);
-                logger.log(Level.FINE, "Magnifying glass found {0} solution(s). At time: {1}", new Object[] {Integer.valueOf(solutionsFound), Integer.valueOf((int)((System.currentTimeMillis()-startTime)/1000))});
-            }
-
-            // Find another starting point
-            solver = IncrementalSolver.solver(getOptions());
-            exclusionConstraints.add(currentValues.exclusionConstraint());
-
-            constraint = Formula.and(exclusionConstraints);
-            solution = solver.solve(constraint, problem.getBounds());
-            incrementStats(solution, problem, constraint, false, null);
-
-            //count this step but first go to new index because it's a new base point
-            counter.nextIndex();
-            counter.countStep();
-        }
-        logger.log(Level.FINE, "All Pareto points found. At time: {0}", Integer.valueOf((int)(System.currentTimeMillis()-startTime)/1000));
+        // Wait for the thread to return
+        pool.invoke(task);
 
         end(notifier);
         debugWriteStatistics();
@@ -204,6 +157,92 @@ public class PartitionedGuidedImprovementAlgorithm extends MultiObjectiveAlgorit
             }
             incrementalSolver.free();
             return currentValues.boundaryConstraint(objective);
+        }
+
+    }
+
+    // RecursiveAction is a ForkJoinTask that doesn't return a result
+    // One potential issue is that we insert solutions into a blocking queue and use a shared counter;
+    // this is something that should be minimized (or eliminated) from ForkJoinTasks.
+    private class PartitionSpaceTask extends RecursiveAction {
+
+        private static final long serialVersionUID = 1L;
+        private final MultiObjectiveProblem problem;
+        private Formula boundaryConstraints;
+        private final SolutionNotifier notifier;
+
+        public PartitionSpaceTask(MultiObjectiveProblem problem, Formula boundaryConstraints, SolutionNotifier notifier) {
+            this.problem = problem;
+            this.boundaryConstraints = boundaryConstraints;
+            this.notifier = notifier;
+        }
+
+        @Override
+        protected void compute() {
+            // Throw the dart within our partition
+            IncrementalSolver solver = IncrementalSolver.solver(getOptions());
+            Formula constraints = problem.getConstraints().and(boundaryConstraints);
+            Solution solution = solver.solve(constraints, problem.getBounds());
+            incrementStats(solution, problem, constraints, false, constraints);
+
+            // If unsat, then we're done, so return without splitting
+            if (!isSat(solution)) {
+                return;
+            }
+
+            //count this step but first go to new index because it's a new base point
+            // TODO: Semantics are wrong if this is the first task computed. We don't want to call nextIndex() yet
+            //counter.nextIndex();
+            //counter.countStep();
+
+            // Work up to the Pareto front
+            MetricPoint currentValues = null;
+            Solution previousSolution = null;
+            while (isSat(solution)) {
+              currentValues = MetricPoint.measure(solution, problem.getObjectives(), getOptions());
+              logger.log(Level.FINE, "Found a solution. At time: {0}, Improving on {1}", new Object[] { Integer.valueOf((int)(System.currentTimeMillis()-startTime)/1000), currentValues.values() });
+
+              final Formula improvementConstraints = currentValues.parametrizedImprovementConstraints();
+
+              previousSolution = solution;
+              solution = solver.solve(improvementConstraints, new Bounds(problem.getBounds().universe()));
+              incrementStats(solution, problem, improvementConstraints, false, improvementConstraints);
+
+              //counter.countStep();
+            }
+            foundParetoPoint(currentValues);
+
+            // Free the solver's resources since we will be creating a new solver
+            solver.free();
+
+            if (!options.allSolutionsPerPoint()) {
+                // no magnifying glass
+                // previous solution was on the pareto front: report it
+                tell(notifier, previousSolution, currentValues);
+            } else {
+                // magnifying glass
+                final Collection<Formula> assignmentsConstraints = currentValues.assignmentConstraints();
+                assignmentsConstraints.add(problem.getConstraints());
+                int solutionsFound = magnifier(Formula.and(assignmentsConstraints), problem.getBounds(), currentValues, notifier);
+                logger.log(Level.FINE, "Magnifying glass found {0} solution(s). At time: {1}", new Object[] {Integer.valueOf(solutionsFound), Integer.valueOf((int)((System.currentTimeMillis()-startTime)/1000))});
+            }
+
+            // Now we can split the search space based on the Pareto point and create new tasks
+            // For n metrics, we want all combinations of m_i <= M_i and m_i >= M_i where M_i is the current value
+            // To iterate over this, we map the bits of a bitset to the different combinations of metrics
+            // We skip 0 (the partition that is already dominated) and 2^n (the partition where we didn't find any solutions)
+            // Store the tasks in a list so we can call invokeAll() on the entire list
+            List<PartitionSpaceTask> partitionTasks = new ArrayList<PartitionSpaceTask>();
+            int maxMapping = (int) Math.pow(2, problem.getObjectives().size());
+            for (int mapping = 1; mapping < maxMapping; mapping++) {
+                BitSet set = BitSet.valueOf(new long[] { mapping });
+                // Get the constraints for this particular mapping, and add to a list of tasks
+                Formula constraint = boundaryConstraints.and(currentValues.partitionConstraints(set));
+                partitionTasks.add(new PartitionSpaceTask(problem, constraint, notifier));
+            }
+
+            // Fork and join all the tasks
+            invokeAll(partitionTasks);
         }
 
     }
