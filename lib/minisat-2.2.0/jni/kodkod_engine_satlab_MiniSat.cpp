0829aba89b6f7ab31ef5e882eb3ddbcd08de1ca6
Christopher Kleynhans <chris@kleynhans.ca>
2013-07-19 00:53:46 -0400

    Random seed must be positive.

--- lib/minisat-2.2.0/jni/kodkod_engine_satlab_MiniSat.cpp
+++ lib/minisat-2.2.0/jni/kodkod_engine_satlab_MiniSat.cpp
@@ -29,6 +29,11 @@ JNIEXPORT jlong JNICALL Java_kodkod_engine_satlab_MiniSat_make
   Solver* solver = new Solver();
   solver->rnd_init_act = true;
   solver->random_seed = (solver - (Solver*)(0));
+  if (solver->random_seed < 0) {
+    solver->random_seed *= -1;
+  } else if (solver->random_seed == 0) {
+    solver->random_seed += 1;
+  }
 
   return ((jlong) solver);
 }
