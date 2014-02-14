21be77641175ada15b7f497bcbda808adc62d555
Christopher Kleynhans <chris@kleynhans.ca>
2014-02-14 00:40:13 -0500

    Use IGIA.

--- src/kodkod/multiobjective/MultiObjectiveSolver.java
+++ src/kodkod/multiobjective/MultiObjectiveSolver.java
@@ -16,7 +16,6 @@ import kodkod.engine.fol2sat.UnboundLeafException;
 import kodkod.instance.Bounds;
 import kodkod.multiobjective.algorithms.IncrementalGuidedImprovementAlgorithm;
 import kodkod.multiobjective.algorithms.MultiObjectiveAlgorithm;
-import kodkod.multiobjective.algorithms.OverlappingGuidedImprovementAlgorithm;
 import kodkod.multiobjective.concurrency.BlockingSolutionIterator;
 import kodkod.multiobjective.concurrency.SolutionNotifier;
 import kodkod.multiobjective.concurrency.TranslatingBlockingQueueSolutionNotifier;
@@ -40,7 +39,7 @@ public final class MultiObjectiveSolver implements KodkodSolver {
 
 		solutionQueue = new LinkedBlockingQueue<Solution>();
 		solutionIterator = new BlockingSolutionIterator(solutionQueue);
-		algorithm = new OverlappingGuidedImprovementAlgorithm("OGIA", options);
+		algorithm = new IncrementalGuidedImprovementAlgorithm("IGIA", options);
 		solutionNotifier = new TranslatingBlockingQueueSolutionNotifier(solutionQueue);
 	}
 	
