8184c1951e6197c77a7fd4a8fa0cd199a39d25a4
Ming-Ho Yee <mh@mhyee.com>
2014-04-30 20:08:01 -0400

    Remove unused imports

--- src/kodkod/multiobjective/algorithms/PartitionedGuidedImprovementAlgorithm.java
+++ src/kodkod/multiobjective/algorithms/PartitionedGuidedImprovementAlgorithm.java
@@ -8,16 +8,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CompletionService;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -52,7 +45,7 @@ public class PartitionedGuidedImprovementAlgorithm extends MultiObjectiveAlgorit
         begin();
 
         final List<Formula> exclusionConstraints = new ArrayList<Formula>();
-        
+
         exclusionConstraints.add(problem.getConstraints());
 
         // Disable MetricPoint logger temporarily
